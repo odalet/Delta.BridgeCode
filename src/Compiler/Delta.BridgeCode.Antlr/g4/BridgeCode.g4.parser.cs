17129ca7f8101e22a60fdf027de4861ab3ba0955
Olivier DALET <odalet@gmail.com>
2015-05-31 22:05:56 +0200

    comment

--- src/Compiler/Delta.BridgeCode.Antlr/g4/BridgeCode.g4.parser.cs
+++ src/Compiler/Delta.BridgeCode.Antlr/g4/BridgeCode.g4.parser.cs
@@ -12,6 +12,7 @@ namespace Delta.BridgeCode.Antlr
         /// <param name="e">The recognition exception.</param>
         public override void NotifyErrorListeners(IToken offendingToken, string msg, RecognitionException e)
         {
+            // Does nothing, but allows for breakpoints...
             base.NotifyErrorListeners(offendingToken, msg, e);
         }
     }
